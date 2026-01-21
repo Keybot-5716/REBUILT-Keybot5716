d5adf59086a0e99316fd6834289ef567b0ca7389
Isju152 <isjuamador@gmail.com>
2026-01-20 18:02:27 -0600

    First look at simulating intake

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeIOSim.java
@@ -0,0 +1,57 @@
+package frc.robot.subsystems.intake;
+
+import static edu.wpi.first.units.Units.Meters;
+
+import org.ironmaple.simulation.IntakeSimulation;
+import org.ironmaple.simulation.drivesims.AbstractDriveTrainSimulation;
+
+public class IntakeIOSim implements IntakeIO {
+  private final IntakeSimulation intakeSimulation;
+
+  public IntakeIOSim(AbstractDriveTrainSimulation driveTrain) {
+    this.intakeSimulation =
+        IntakeSimulation.OverTheBumperIntake(
+            // Este es el tipo de objeto que el intake va a recoger
+            "Ball",
+            // Specify the drivetrain to which this intake is attached
+            driveTrain,
+            // The width of the intake (CHANGEEEEE)
+            Meters.of(0.508), // 20 inches
+            // The extension length of the intake beyond the robot's frame (when activated)
+            Meters.of(0.2),
+            // The intake is mounted on the front? side of the chassis
+            IntakeSimulation.IntakeSide.FRONT,
+            // The intake can hold up to ? balls?
+            100);
+  }
+
+  public void setRunning(boolean runIntake) {
+    if (runIntake)
+      intakeSimulation
+          .startIntake(); // Extends the intake out from the chassis frame and starts detecting
+    // contacts with game pieces
+    else
+      intakeSimulation
+          .stopIntake(); // Retracts the intake into the chassis frame, disabling game piece
+    // collection
+  }
+
+  public boolean isAnyBallInsideIntake() {
+    return intakeSimulation.getGamePiecesAmount()
+        != 0; // True if there is a game piece in the intake
+  }
+
+  public void launchBall() {
+    // if there is a ball in the intake, it will be removed and return true; otherwise, returns
+    // false
+    if (intakeSimulation.obtainGamePieceFromIntake()) {}
+    // ShooterIOSim.launchNote(); //notify the simulated flywheels to launch a ball (We need the
+    // shooter to be aware of this)
+  }
+
+  @Override
+  public void updateInputs(IntakeIOInputs inputs) {}
+
+  @Override
+  public void setVoltage(double voltage) {}
+}

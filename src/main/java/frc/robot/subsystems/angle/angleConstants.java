4add07f9ed2a5ea53d6f12e83c7835dbeee843b6
Jorge-Ceron <a01278485@tec.mx>
2026-01-27 15:08:37 -0600

    Constants and TalonFX added

---
+++ src/main/java/frc/robot/subsystems/angle/angleConstants.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.angle;
+
+public class angleConstants {
+  public static final int ANGLE_ID = 21;
+  public static final double GEARBOX_REDUCTION = 160; // 5:4:3:2.66666:1
+  public static final double MIN_OFFSET = 0.1;
+  public static final double NONE = 3;
+
+}

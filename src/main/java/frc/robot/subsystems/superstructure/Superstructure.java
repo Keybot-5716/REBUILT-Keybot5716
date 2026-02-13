c9cf93b780580ff8ea7888e4f5a8cffb1b43d877
Spartan7182 <alumnoruben3b@gmail.com>
2026-02-13 00:27:59 -0600

    A

--- src/main/java/frc/robot/subsystems/superstructure/Superstructure.java
+++ src/main/java/frc/robot/subsystems/superstructure/Superstructure.java
@@ -1,3 +1,28 @@
 package frc.robot.subsystems.superstructure;
 
-public class Superstructure {}
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.intake.IntakeSubsystem;
+import frc.robot.subsystems.shooter.ShooterSubsystem;
+import frc.robot.subsystems.transfer.TransferSubsystem;
+
+public class Superstructure extends SubsystemBase {
+
+    private final IntakeSubsystem intakeSub;
+    private final TransferSubsystem transferSub;
+    private final ShooterSubsystem shooterSub;
+
+    private SuperstructureStates currentState = SuperstructureStates.DEFAULT;
+    private SuperstructureStates desiredState = SuperstructureStates.DEFAULT;
+
+    public Superstructure(IntakeSubsystem intakeSub, TransferSubsystem transferSub, ShooterSubsystem shooterSub) {
+        this.intakeSub = intakeSub;
+        this.transferSub = transferSub;
+        this.shooterSub = shooterSub;
+    }
+
+    @Override
+    public void periodic() {
+        currentState = desiredState;
+    }
+
+}
